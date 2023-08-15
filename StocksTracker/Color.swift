fb37f86a540da5ba664b0c3543c4a2ae7e879b40
KAARTHIKEYA K <kaarthikeyak@KAARTHIKEYAs-MacBook-Air.local>
2023-08-15 17:39:04 +0530

    Initial Commit with Custom Color Theme

---
+++ StocksTracker/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  StocksTracker
+//
+//  Created by KAARTHIKEYA K on 15/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
---
+++ StocksTracker/StocksTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksTrackerApp.swift
+//  StocksTracker
+//
+//  Created by KAARTHIKEYA K on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
