109afb260a7e82644b46f966b1f5e5fabf1e644a
kaarthikeyak <kaarthu2003@gmail.com>
2023-08-21 14:38:50 +0530

    feat : Networking Manager

---
+++ StocksTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkingManager.swift
+//  PaperCoin
+//
+//  Created by KAARTHIKEYA K on 21/08/23.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error>{
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>){
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
