e667c5702059115acc836cfeb060d9b151815d80
kaarthikeyak <kaarthu2003@gmail.com>
2023-08-30 23:26:01 +0530

    feat : searchText Subscriber added.

--- StocksTracker/Core/Home/ViewModels/HomeViewViewModel.swift
+++ StocksTracker/Core/Home/ViewModels/HomeViewViewModel.swift
@@ -12,6 +12,7 @@ class HomeViewViewModel : ObservableObject {
     
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
+    
     @Published var searchText: String = ""
     
     private let dataService = CoinDataService()
@@ -22,10 +23,29 @@ class HomeViewViewModel : ObservableObject {
     }
     
     func addSubscribers(){
+        
         dataService.$allCoins
             .sink(receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             })
+            .store(in: &cancellables)
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [Coin] in
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                let lowercasedText = text.lowercased()
+                return startingCoins.filter { coin -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+            }
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
             .store(in: &cancellables)
     }
     
