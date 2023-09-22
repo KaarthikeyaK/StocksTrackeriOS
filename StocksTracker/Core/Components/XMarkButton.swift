4d74aef46cbc70315fbe0d7f64a1b0e7a54c3b18
kaarthikeyak <kaarthu2003@gmail.com>
2023-09-22 12:30:38 +0530

    CoinLogoView Created.

---
+++ StocksTracker/Core/Components/XMarkButton.swift
@@ -0,0 +1,20 @@
+//
+//  XMarkButton.swift
+//  PaperCoin
+//
+//  Created by KAARTHIKEYA K on 17/09/23.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}
---
+++ StocksTracker/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioView.swift
+//  PaperCoin
+//
+//  Created by KAARTHIKEYA K on 16/09/23.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PortfolioView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioView()
+    }
+}
